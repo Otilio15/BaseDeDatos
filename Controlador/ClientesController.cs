62172f746ef90672f6ccd1acb4435961641d5433
Otilio15 <54569333+Otilio15@users.noreply.github.com>
2019-10-21 21:49:06 -0600

    (PARQUEO)
    Primer avance

---
+++ Controlador/ClientesController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+
+
+
+namespace Controlador
+{
+   public class ClientesController
+    {
+
+        PARQUEOEntities db = new PARQUEOEntities();
+
+        public bool GuardarCliente(int id, string nombre, string cedula )
+        {
+            bool resultado = false;
+
+            try
+            {
+                CLIENTE cliente = new CLIENTE();
+
+                cliente.IDcliente = id;
+                cliente.Nombre = nombre;
+                cliente.Cedula= cedula;
+                db.CLIENTES.Add(cliente);
+                db.SaveChanges();
+
+                System.Data.Entity.Core.Objects.ObjectParameter mensaje = new System.Data.Entity.Core.Objects.ObjectParameter("Mensaje","");
+                db.GenereaTiquete("user", id, "1212DDD", mensaje );
+
+               
+
+                return resultado = true;
+            }
+            catch (Exception ex)
+            {
+
+                return resultado = false;
+                throw;
+            }
+
+
+            return resultado;
+        }
+
+
+        public bool ActualizarCliente(int id, string nombre, string cedula)
+        {
+            bool resultado = false;
+
+            try
+            {
+                CLIENTE cliente = new CLIENTE();
+
+                cliente = (from A in db.CLIENTES where A.IDcliente == id select A).First();
+
+                cliente.Nombre = nombre;
+                cliente.Cedula = cedula;
+                db.SaveChanges();
+
+                return resultado = true;
+            }
+            catch (Exception ex)
+            {
+
+                return resultado = false;
+                throw;
+            }
+
+
+            return resultado;
+        }
+
+        public bool EliminarCliente(string cedula)
+        {
+            bool resultado = false;
+
+            try
+            {
+                CLIENTE cliente = new CLIENTE();
+
+
+                cliente= (from A in db.CLIENTES where A.Cedula == cedula select A).First();
+
+                db.CLIENTES.Remove(cliente);
+                db.SaveChanges();
+
+                return resultado = true;
+            }
+            catch (Exception ex)
+            {
+
+                return resultado = false;
+                throw;
+            }
+
+
+            return resultado;
+        }
+
+        public CLIENTE RetornarCliente(string cedula)
+        {
+            CLIENTE cliente = new CLIENTE();
+            
+            try
+            {
+                cliente = (from A in db.CLIENTES where A.Cedula == cedula select A).First();
+            }
+            catch (Exception ex)
+            {
+                return cliente = null;
+                throw;
+            }
+            return cliente;
+        }
+    }
+}
---
+++ Controlador/VehiculosController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Modelo;
+
+
+namespace Controlador
+{
+    public class VehiculosController
+    {
+        PARQUEOEntities db = new PARQUEOEntities();
+
+        public bool GuardarVehículo (string placa, string marca, int modelo)
+        {
+            bool resultado = false;
+
+            try
+            {
+                VEHICULO Carro = new VEHICULO();
+
+                Carro.Marca = marca;
+                Carro.Modelo = modelo;
+                Carro.NumeroPlaca = placa;
+                db.VEHICULOS.Add(Carro);
+                db.SaveChanges();
+
+                return resultado = true;
+            }
+            catch (Exception ex)
+            {
+                
+                return resultado = false;
+                throw;
+            }
+
+
+            return resultado;
+        }
+
+
+        public bool ActualizarVehículo(string placa, string marca, int modelo)
+        {
+            bool resultado = false;
+
+            try
+            {
+                VEHICULO Carro = new VEHICULO();
+
+                Carro = (from A in db.VEHICULOS where A.NumeroPlaca == placa select A).First();
+
+                Carro.Marca = marca;
+                Carro.Modelo = modelo;
+                db.SaveChanges();
+
+                return resultado = true;
+            }
+            catch (Exception ex)
+            {
+
+                return resultado = false;
+                throw;
+            }
+
+
+            return resultado;
+        }
+
+        public bool EliminarVehículo(string placa)
+        {
+            bool resultado = false;
+
+            try
+            {
+                VEHICULO Carro = new VEHICULO();
+
+                Carro = (from A in db.VEHICULOS where A.NumeroPlaca == placa select A).First();
+
+                db.VEHICULOS.Remove(Carro);
+                db.SaveChanges();
+
+                return resultado = true;
+            }
+            catch (Exception ex)
+            {
+
+                return resultado = false;
+                throw;
+            }
+
+
+            return resultado;
+        }
+
+        public VEHICULO RetornarVehiculo(string placa)
+        {
+            VEHICULO Carro = new VEHICULO();
+            try
+            {
+                Carro = (from A in db.VEHICULOS where A.NumeroPlaca == placa select A).First();
+                return Carro;
+            }
+            catch (Exception ex)
+            {
+                return Carro = null;
+                throw;
+            }
+            return Carro;
+        }
+    }
+
+
+   
+}
